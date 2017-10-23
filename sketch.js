e346327a0e6986c808776ebf128736e93e1c9ca4
johnlyden <john.lyden86@gmail.com>
2017-10-23 10:25:09 -0500

    added slider and updated colors

--- sketch.js
+++ sketch.js
@@ -1,10 +1,23 @@
 // mandelbrot equation (a*a - b*b) + 2abi
 
-function setup() { 
-  createCanvas(360, 360);
-  pixelDensity(1);
-  loadPixels();
+var minVal = -0.5;
+var maxVal = .5;
 
+var minSlider;
+var maxSlider;
+
+function setup() { 
+  createCanvas(300, 300);
+  pixelDensity(1);
+
+  minSlider = createSlider(-2.5, 0, -2.5, 0.1); //min value to be some value between -2.5 and 0.  start with -2.5 and have an increment value of .1
+  maxSlider = createSlider(0, 2.5, 2.5, .1);
+}
+
+function draw() {
+  // set max iterations we want to run
+  var maxIterations = 100;
+  loadPixels()
   // looking at every x and y value and setting it's pixel to a grayscale value of 51
 
   // inside this code, for every one of these x and y values, run this mandelbrot set math thing and see if the values are getting bigger or not and set thier valu
@@ -16,9 +29,9 @@ function setup() {
       // mapping all complex numbers over this range
       // -2 - 2i all the way to 2 + 2i
       // goes from 0 to width  to -2 and 2
-      var a = map(x, 0, width, -2, 2);
+      var a = map(x, 0, width, minSlider.value(), maxSlider.value());
       // goes from 0 to height also to -2 and 2
-      var b = map(y, 0, height, -2, 2);
+      var b = map(y, 0, height, minSlider.value(), maxSlider.value());
 
       // start our iteration
       // original a and b
@@ -29,7 +42,7 @@ function setup() {
       // var z = 0;
 
       // mandelbrot equation (a*a - b*b) + 2abi
-      while (n < 100) {
+      while (n < maxIterations) {
         // first thing we need to do is calculate a*a - b*b
         // figuring out what z*z is here 
         var aa = a*a - b*b;
@@ -48,10 +61,15 @@ function setup() {
         n++;
       }
 
-      var bright = map(n, 0, 100, 0, 255);
+      var bright = map(n, 0, maxIterations, 0, 1);
+      bright = map(sqrt(bright), 0, 1, 0, 255);
       // if (n === 100) {
       //   bright = 255;
       // }
+      // if n gets to max iterations
+      if (n == maxIterations) {
+        bright = 0;
+      }
 
       var pix = (x + y * width) * 4;
       pixels[pix + 0] = bright;
