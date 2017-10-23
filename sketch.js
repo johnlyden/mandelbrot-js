774b6ee48350efa5f601283d41fa8debe5d990f6
johnlyden <john.lyden86@gmail.com>
2017-10-23 09:48:47 -0500

    added loop to change pixel value to grayscale 51

--- sketch.js
+++ sketch.js
@@ -1,7 +1,17 @@
 function setup() { 
-  createCanvas(400, 400);
-} 
+  createCanvas(360, 240);
+  pixelDensity(1);
+  loadPixels();
 
-function draw() { 
-  background(220);
+  // looking at every x and y value and setting it's pixel to a grayscale value of 51
+  for (var x = 0; x < width; x++) {
+    for (var y = 0; y < height; y++) {
+      var pix = (x + y * width) * 4;
+      pixels[pix + 0] = 51;
+      pixels[pix + 1] = 51;
+      pixels[pix + 2] = 51;
+      pixels[pix + 3] = 255;
+    }
+  }
+  updatePixels();
 }
